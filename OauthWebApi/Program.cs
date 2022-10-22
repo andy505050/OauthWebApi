a2442bbfeacff07d75b87b41510e44911ae2a4dc
SadGuy <alex60510@hotmail.com>
2022-10-23 01:00:02 +0800

    UseStaticFiles

--- OauthWebApi/Program.cs
+++ OauthWebApi/Program.cs
@@ -22,6 +22,8 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseStaticFiles();
+
 app.MapGet("/LineLoginSetting", (IOptions<LineLoginConfig> lineLoginConfigOptions) => new
 {
     lineLoginConfigOptions.Value.ClientId, 
